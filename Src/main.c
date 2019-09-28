645038b38726f56206804313cd14e5278df7c250
Wouter Brookhuis <wrdbrookhuis@gmail.com>
2019-09-28 18:16:00 +0200

    Some PPU debug info displaying

--- Src/main.c
+++ Src/main.c
@@ -161,9 +161,11 @@ static bool Update(float deltaTime)
 {
   CPU_t *cpu;
   Bus_t *bus;
+  PPU_t *ppu;
   const InstructionTableEntry_t *instr;
   cpu = NES_GetCPU();
   bus = NES_GetBus();
+  ppu = NES_GetPPU();
 
   instr = InstructionTable_GetInstruction(cpu->Instruction);
 
@@ -178,18 +180,27 @@ static bool Update(float deltaTime)
            );
 
   // Second row: CPU status
+//  snprintf(&_statusBarBuffer[STATUS_BAR_CHARS_PER_ROW],
+//          STATUS_BAR_CHARS_PER_ROW + 1,
+//          "%04X: %3s %3s A:%02X X:%02X Y:%02X S:%02X P:%02X, C:%010d",
+//          cpu->InstructionPC,
+//          instr->Name,
+//          AddressingMode_GetName(instr->AddressingMode),
+//          cpu->A,
+//          cpu->X,
+//          cpu->Y,
+//          cpu->S,
+//          cpu->P,
+//          cpu->CycleCount
+//          );
+  // Second row: PPU status
   snprintf(&_statusBarBuffer[STATUS_BAR_CHARS_PER_ROW],
           STATUS_BAR_CHARS_PER_ROW + 1,
-          "%04X: %3s %3s A:%02X X:%02X Y:%02X S:%02X P:%02X, C:%010d",
-          cpu->InstructionPC,
-          instr->Name,
-          AddressingMode_GetName(instr->AddressingMode),
-          cpu->A,
-          cpu->X,
-          cpu->Y,
-          cpu->S,
-          cpu->P,
-          cpu->CycleCount
+          "H:%03d V:%03d CTRL:%02X STAT:%02X",
+          ppu->HCount,
+          ppu->VCount,
+          ppu->Ctrl,
+          ppu->Status
           );
   uint16_t memAddress = cpu->InstructionPC - HALF_MEM_WINDOW_SIZE;
   for (int i = 0; i < HALF_MEM_WINDOW_SIZE * 2 + 1; i++)
